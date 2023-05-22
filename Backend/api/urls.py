765743e0b15a4be0d449246f1ff1b7c47d9a4343
Farhan Tahmid <farhantahmid881@gmail.com>
2023-05-22 16:51:45 +0600

    login signup complete

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from api.views import RegisterUser
+from api.views import RegisterUsers,Login
 
 
 app_name='api'
 
 urlpatterns = [
-    path('signup', RegisterUser.as_view(), name='signup'),
+    path('signup', RegisterUsers.as_view(), name='signup'),
+    path('login',Login.as_view(),name='login')
 ]
